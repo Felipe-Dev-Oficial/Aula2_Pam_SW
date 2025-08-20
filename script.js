67810d2818558dd0e5346430e56fcd2ca6bc4bb7
Felipe-Dev-Oficial <duartefelipe020@gmail.com>
2025-08-20 20:42:18 -0300

    deploy

---
+++ script.js
@@ -0,0 +1,9 @@
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("/Aula2_Pam_SW/sw.js")
+    .then((registration)=>{
+        console.log("Service Worker registration sucessful!", registration);
+    })
+    .catch((error)=>{
+        console.log("Error trying register Service Worker", error)
+    });    
+}
---
+++ sw.js
@@ -0,0 +1,15 @@
+self.addEventListener("install", (event) =>{
+    caches.open("asd").then((cache) =>{
+        console.log("cahce opened")
+        cache.addAll("index.html", "manifest.json", "script.js","sw.js")
+
+    });
+});
+        self.addEventListener("fetch",(event)=>{
+            event.respondWith(
+                caches.match(EventTarget.request).then((response)=>{
+                    return response || 
+                    fetch(event.request);
+                })
+            )
+        });
